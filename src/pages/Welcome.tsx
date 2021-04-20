aa454221a3f634a2679909dd152e61da6890b4e1
Pedro Sena <pmfsl@cin.ufpe.br>
2021-04-20 01:27:18 -0300

    project init

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { SafeAreaView, Text, Image, StyleSheet } from "react-native";
+import Button from "../components/Button";
+import wateringImg from "../assets/watering.png";
+import colors from "../styles/colors";
+
+export default function Welcome() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.title}>
+        Gerencie {"\n"}
+        suas plantas {"\n"}
+        de forma fácil
+      </Text>
+      <Image source={wateringImg} style={styles.image} />
+      <Text style={styles.subtitle}>
+        {" "}
+        Não esqueça mais de regar suas plantas. Nós cuidamos de lembrar você
+        sempre que precisar.
+      </Text>
+      <Button title="Próximo" />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    textAlign: "center",
+    color: colors.heading,
+    marginTop: 38,
+  },
+  subtitle: {
+    fontSize: 18,
+    textAlign: "center",
+    color: colors.heading,
+    paddingHorizontal: 20,
+  },
+  image: {
+    width: 292,
+    height: 284,
+  },
+});
